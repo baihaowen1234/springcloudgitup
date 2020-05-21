7de591ef5790bb02d206b19dfbcefc755f3f99b3
Administrator <1416196787@qq.com>
2020-05-21 17:07:48 +0800

    17.07提交的信息

--- cloud-consumerconsul-order80/src/test/java/T1.java
+++ cloud-consumerconsul-order80/src/test/java/T1.java
@@ -1,9 +1,6 @@
 public class T1 {
     public static void main(String[] args) {
         System.out.println(1);
-        System.out.println("分支");
-        System.out.println("主干");
-        System.out.println("gitup上面的修改");
- System.out.println("gi1111改");
+
     }
 }
